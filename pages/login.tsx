965f1bb7998fb4e678068fc015294d97fd713f22
Argha Chakrabarty <ac380012@gmail.com>
2023-05-12 22:45:42 +0530

    update login issues

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,20 @@
 import { signInWithGoogle } from '@/components/firebase';
 import { Inter } from 'next/font/google'
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Auth() {
+  const router = useRouter()
   return (
     <main
       className={`min-h-screen ${inter.className}`}
     >
-      <button onClick={signInWithGoogle} className="p-10 bg-red-300">
+      <button onClick={
+        () => {
+          signInWithGoogle(); router.push("/profile")
+        }
+      } className="p-10 bg-red-300">
         Sign In With Google
       </button>
     </main>
