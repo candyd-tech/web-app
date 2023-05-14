ec3c0e63c6d530ceb32be27c4a0cc996de889529
Argha Chakrabarty <ac380012@gmail.com>
2023-05-14 17:45:14 +0530

    login page revamp

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,40 @@
 import { signInWithGoogle } from '@/components/firebase';
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import { useRouter } from 'next/router';
+import { FaGoogle } from 'react-icons/fa';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  subsets: [ 'latin' ]
+});
+
 
 export default function Auth() {
   const router = useRouter()
   return (
     <main
-      className={`min-h-screen ${inter.className}`}
+      className={`bg-[url(/login_bg.jpg)] bg-cover min-h-screen
+      ${inter.className} flex items-center justify-center `}
     >
-      <button onClick={
-        () => {
-          signInWithGoogle(); router.push("/profile")
-        }
-      } className="p-10 bg-red-300">
-        Sign In With Google
-      </button>
+      <div className="p-5 flex flex-col h-full gap-10
+        items-center justify-center bg-blue-100 bg-opacity-50 "
+      >
+        <h1 className={`font-bold text-center`}>Join us now to relive the magic of your campus days by capturing and sharing your cherished memories through daily fun prompts, and stay tuned for upcoming features and exciting prizes!</h1>
+        <button onClick={
+          () => {
+            signInWithGoogle(); router.push("/profile")
+          }} className="px-5 bg-[#5c95e3]
+            flex items-center gap-4 shadow-gray-700
+            shadow-sm rounded-md"
+        >
+          <div className="h-full py-3">
+            <p> <FaGoogle /> </p>
+          </div>
+          <p className="py-3">
+            Sign In With Google
+          </p>
+        </button>
+      </div>
     </main>
   );
 }
