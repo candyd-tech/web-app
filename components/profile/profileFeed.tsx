5aad24b816bb0a6cc71cd32945c5f9eec8f55470
Argha Chakrabarty <ac380012@gmail.com>
2023-05-14 15:34:14 +0530

    [0] fix

--- components/profile/profileFeed.tsx
+++ components/profile/profileFeed.tsx
@@ -30,7 +30,7 @@ const GalleryPhotos = ({post_id}: {post_id: string}) => {
     axios.get(`${process.env.NEXT_PUBLIC_DB_URL}/v1/post/${post_id}`)
       .then(res => {
         setPost(res.data)
-        axios.get(`${process.env.NEXT_PUBLIC_DB_URL}/v1/media/${res.data.medias[0]}`)
+        res.data && axios.get(`${process.env.NEXT_PUBLIC_DB_URL}/v1/media/${res.data.medias[0]}`)
           .then(r => {
             setMedia(r.data);
             setLoading(false)
